b5d8bbce73e67848be3c018260d95cfdedc2df01
Kien Ngo <trungkien08072000@gmail.com>
2023-10-04 17:30:18 +0700

    update

--- src/main/java/com/udacity/critter/entities/Pet.java
+++ src/main/java/com/udacity/critter/entities/Pet.java
@@ -10,13 +10,18 @@ import java.time.LocalDate;
 @Entity
 @Data
 public class Pet implements Serializable {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Enumerated(EnumType.STRING)
     private PetType type;
+
     private String name;
 
     @ManyToOne(optional = false)
+    @JoinColumn(name = "customer_id")
     private Customer customer;
 
     private LocalDate birthDate;
